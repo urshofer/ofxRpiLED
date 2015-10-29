d247b11d6b43fa423b6b3f6d429fcf67bbc299ba
urshofer <u.hofer@tr51.org>
2015-10-29 12:11:58 +0100

    changed default, added image interface and default setup

--- src/ofxRpiLED.cpp
+++ src/ofxRpiLED.cpp
@@ -4,7 +4,7 @@
 // Constructor
 ofxRpiLED::ofxRpiLED() {
 	rows = 32;    // A 32x32 display. Use 16 when this is a 16x32 display.
-	chain = 1;    // Number of boards chained together.
+	chain = 2;    // Number of boards chained together.
 	parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
 }
 
@@ -14,41 +14,53 @@ ofxRpiLED::~ofxRpiLED() {
 	delete canvas;
 }
 
-void ofxRpiLED::setup(int _rows, int _chain, int _parallel){
+void ofxRpiLED::setup() {
 	/*
 	* Set up GPIO pins. This fails when not running as root.
 	*/
 	if (!io.Init())
 		return;
 
-	rows 		= _rows;
-	chain 		= _chain;
-	parallel 	= _parallel;
 	canvas = new RGBMatrix(&io, rows, chain, parallel);
-	/* Some Test */
-	canvas->Fill(255,0,0);	
-	ofSleepMillis(100);
-	canvas->Fill(0,255,0);		
-	ofSleepMillis(100);
-	canvas->Fill(0,0,255);	
-	ofSleepMillis(100);
+
+	/* 
+	* Clear and cache size 
+	*/
+	
 	canvas->Fill(0,0,0);
 	cW = canvas->width();
 	cH = canvas->height();
+}
+
+void ofxRpiLED::setup(int _rows, int _chain, int _parallel){
+	rows 		= _rows;
+	chain 		= _chain;
+	parallel 	= _parallel;
+	setup();
 }
 
 void ofxRpiLED::clear(){
 	canvas->Clear();
 }
 
-void ofxRpiLED::draw(ofPixels p){
-	if (cW != p.getWidth() && cH != p.getHeight())
-		p.resize(cW, cH);
-    for (int x = 0; x < cW; x++) {
-		for (int y = 0; y < cH; y++) {
+void ofxRpiLED::draw(ofPixels &p){
+	int w = cW > p.getWidth()  ? cW : p.getWidth();
+	int h = cH > p.getHeight() ? cH : p.getHeight();	
+	for (int x = 0; x < w; x++) {
+		for (int y = 0; y < h; y++) {
 			ofColor c = p.getColor(x, y);
 			canvas->SetPixel(x, y, c.r, c.g, c.b);
 		}
-    }	
+	}	
+}
 
+void ofxRpiLED::draw(ofImage &i){
+	int w = cW > i.getWidth()  ? cW : i.getWidth();
+	int h = cH > i.getHeight() ? cH : i.getHeight();	
+	for (int x = 0; x < w; x++) {
+		for (int y = 0; y < h; y++) {
+			ofColor c = i.getColor(x, y);
+			canvas->SetPixel(x, y, c.r, c.g, c.b);
+		}
+	}	
 }
--- src/ofxRpiLED.h
+++ src/ofxRpiLED.h
@@ -8,22 +8,20 @@ using rgb_matrix::Canvas;
 class ofxRpiLED {
     
 private:
-	
-    int 		rows = 32;    // A 32x32 display. Use 16 when this is a 16x32 display.
-    int 		chain = 1;    // Number of boards chained together.
-    int 		parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-    int 		cW;
-    int 		cH;
-    Canvas 		*canvas;
-    GPIO 		io;
+	int 		rows;		// A 32x32 display. Use 16 when this is a 16x32 display.
+	int 		chain;		// Number of boards chained together.
+	int 		parallel;	// Number of chains in parallel (1..3). > 1 for plus or Pi2
+	int 		cW;
+	int 		cH;
+	Canvas 		*canvas;
+	GPIO 		io;
 
 public:
-   
-    ofxRpiLED();
-    ~ofxRpiLED();
-
-    void setup(int _rows, int _chain, int _parallel);
-    void clear();
-    void draw(ofPixels p);
-  	
+	ofxRpiLED();
+	~ofxRpiLED();
+	void setup();
+	void setup(int _rows, int _chain, int _parallel);
+	void clear();
+	void draw(ofImage &i);
+	void draw(ofPixels &p);
 };
